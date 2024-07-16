f6a2ad9967e8b8aa92c8591d560420ecc8df24ff
gryffindor2023 <150682454+gryffindor2023@users.noreply.github.com>
2024-07-16 17:14:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+}
+
+function setup(){
+    canvas = createCanvas(300,300);  
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet Is Initialised");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+}
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300);
+}
+
+
+
+
